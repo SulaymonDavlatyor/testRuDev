9495fe7898b9e4e02a7fc4ac8496590e58ef953e
SulaymonDavlatyor <76821700+SulaymonDavlatyor@users.noreply.github.com>
2021-07-13 14:31:13 +0500

    Add files via upload

---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,86 @@
+package org.example;
+
+public class Product {
+
+        private long productId;
+        private int sellerId;
+        private String oriMinPrice;
+        private String oriMaxPrice;
+        private int promotionId;
+        private String productTitle;
+        private String url;
+
+        public String getUrl() {
+                return url;
+        }
+
+        public long getProductId() {
+                return productId;
+        }
+
+        public void setProductId(int productId) {
+                this.productId = productId;
+        }
+
+        public int getSellerId() {
+                return sellerId;
+        }
+
+        public void setSellerId(int sellerId) {
+                this.sellerId = sellerId;
+        }
+
+        public String getOriMinPrice() {
+                return oriMinPrice;
+        }
+
+        public void setOriMinPrice(String oriMinPrice) {
+                this.oriMinPrice = oriMinPrice;
+        }
+
+        public String getOriMaxPrice() {
+                return oriMaxPrice;
+        }
+
+        public void setOriMaxPrice(String oriMaxPrice) {
+                this.oriMaxPrice = oriMaxPrice;
+        }
+
+        public int getPromotionId() {
+                return promotionId;
+        }
+
+        public void setPromotionId(int promotionId) {
+                this.promotionId = promotionId;
+        }
+
+        public String getProductTitle() {
+                return productTitle;
+        }
+
+        public void setProductTitle(String productTitle) {
+                this.productTitle = productTitle;
+        }
+
+        @Override
+        public String toString() {
+                return "Product{" +
+                        "productId=" + productId +
+                        ", sellerId=" + sellerId +
+                        ", oriMinPrice='" + oriMinPrice + '\'' +
+                        ", oriMaxPrice='" + oriMaxPrice + '\'' +
+                        ", promotionId=" + promotionId +
+                        ", productTitle='" + productTitle + '\'' +
+                        '}';
+        }
+
+        public Product(long productId, int sellerId, String oriMinPrice, String oriMaxPrice, int promotionId, String productTitle, String url) {
+                this.productId = productId;
+                this.sellerId = sellerId;
+                this.oriMinPrice = oriMinPrice;
+                this.oriMaxPrice = oriMaxPrice;
+                this.promotionId = promotionId;
+                this.productTitle = productTitle;
+                this.url = url;
+        }
+}
